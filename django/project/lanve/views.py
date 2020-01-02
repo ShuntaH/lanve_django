38af669a20059fe050d26af0994e35ca4f81a392
shunta <exec.apea@gmail.com>
2020-01-02 19:41:54 +0700

    UPDATE: can display list

--- django/project/lanve/views.py
+++ django/project/lanve/views.py
@@ -1,12 +1,15 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.shortcuts import render
 
-# Create your views here.
 from django.urls import reverse_lazy
 from django.views import generic
 
 from .admin import UserCreationForm
 from .models import Issue
 
+
+# Create your views here.
 
 class WelcomeView(generic.TemplateView):
     template_name = 'lanve/welcome.html'
@@ -22,7 +25,22 @@ class SignupView(generic.CreateView):
         return response
 
 
-class ListView(generic.ListView):
-    model = Issue
-    paginate_by = 8
-    # login_url = '/buybuy/signin/'
+class ListView(generic.ListView, LoginRequiredMixin):
+    model = Issue  # make html file name as model name + list.html
+    paginate_by = 100
+    login_url = 'lanve:signin'
+
+    def get_queryset(self):
+        queryset = Issue.objects.all().order_by('-created_at')  # 新しい投稿順
+        # keyword = self.request.GET.get('keyword')
+        # if keyword:
+        #     queryset = queryset.filter(
+        #         Q(title__icontains=keyword) | Q(text__icontains=keyword)
+        #
+        #     )
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['now'] = timezone.now()
+        return context
