8af0e7920e6fadffc33eec2732178c2737958e6e
shunta <exec.apea@gmail.com>
2020-01-03 15:14:12 +0700

    ADD: signout function

--- django/project/lanve/urls.py
+++ django/project/lanve/urls.py
@@ -9,9 +9,7 @@ urlpatterns = [
     path('', views.WelcomeView.as_view(), name='welcome'),
     path(r'signup/', views.SignupView.as_view(template_name='signup.html'), name='signup'),
     path(r'signin/', auth_views.LoginView.as_view(template_name='signin.html'), name='signin'),
-    # path('signout/', auth_views.LogoutView.as_view(
-    #     template_name='signin.html')
-    #      ),
+    path('signout/', auth_views.LogoutView.as_view(template_name='signout.html'), name='signout'),
     path(r'home/', views.ListView.as_view(), name='list'),
     path(r'add/', views.AddView.as_view(), name='add'),
 ]
