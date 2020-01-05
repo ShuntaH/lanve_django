d57434f6c9a96582d7047c1653fbb1b2690c2013
shunta <exec.apea@gmail.com>
2020-01-06 03:07:46 +0700

    Fix: conflict

--- django/project/lanve/views.py
+++ django/project/lanve/views.py
@@ -1,9 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import ImproperlyConfigured
-from django.db.models import Q
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect
 
 from django.urls import reverse_lazy, reverse
 from django.views import generic
