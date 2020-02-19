3a302aef5ed6c9cb63d0267ca5de83ca1dc825d2
ShuntaH <30917236+ShuntaH@users.noreply.github.com>
2020-02-19 22:48:51 +0700

    Revert "ADD: separated settings.py to local and production for production env…"

--- django/project/project/urls.py
+++ django/project/project/urls.py
@@ -17,11 +17,9 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from django.views.static import serve
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path(r'', include('lanve.urls')),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-              # + path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
